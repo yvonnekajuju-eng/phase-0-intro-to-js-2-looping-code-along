902cc1fe14d8f83356d7001c79337e9ca5e76ab4
yvvonne <yvonnekajuju6@gmail.com>
2025-09-26 15:39:01 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Assignment 1: writeCards()
+function writeCards(names, event) {
+  let messages = []; // new array to hold thank-you messages
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// Assignment 2: countDown()
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--; // decrement to move toward loop exit
+  }
+}
+
